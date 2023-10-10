c8cefd05272186cecb1c192544d20a113c4b7211
Dahir Abdirahman <Dahirmohamedabdi@gmail.com>
2023-10-10 19:49:44 +0200

    commmit 4

--- storageFactory.sol
+++ storageFactory.sol
@@ -12,6 +12,16 @@ contract StorageFactory {
         simpleStorageArray.push(simpleStorage);
     }
 
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // Address
+        // ABI
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
         return simpleStorageArray[_simpleStorageIndex].retrieve();
