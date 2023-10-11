777050fc9e09d3c3f7ab004ac50074390474ceee
Dahir Abdirahman <Dahirmohamedabdi@gmail.com>
2023-10-11 18:10:11 +0200

    2

--- FundMe.sol
+++ FundMe.sol
@@ -19,3 +19,10 @@ contract FundMe {
     constructor() {
         i_owner = msg.sender;
     }
+
+     function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
