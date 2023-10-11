4c2cb6ea64e0d8d8de3ff4a1e73f08577b4178ea
Dahir Abdirahman <Dahirmohamedabdi@gmail.com>
2023-10-11 18:13:22 +0200

    price2

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -19,3 +19,16 @@ library PriceConverter {
         // or (Both will do the same thing)
         // return uint256(answer * 1e10); // 1* 10 ** 10 == 10000000000
     }
+
+    // 1000000000
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // or (Both will do the same thing)
+        // uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1 * 10 ** 18 == 1000000000000000000
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        return ethAmountInUsd;
+    }
+}
